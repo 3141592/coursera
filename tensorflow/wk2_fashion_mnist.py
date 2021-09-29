58bd1b002d72ce6a7483470096d5e2118d803e8f
Roy <roy@Roys-MacBook-Pro.local>
2021-09-28 21:37:06 -0400

    wk2 wip

--- tensorflow/wk2_fashion_mnist.py
+++ tensorflow/wk2_fashion_mnist.py
@@ -6,6 +6,19 @@ from tensorflow import keras
 print(tf.__version__)
 
 mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
+plt.imshow(training_images[42])
+
+training_images = training_images / 255
+test_images = test_images / 255
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, tf.nn.elu),
+                                    tf.keras.layers.Dense(10, tf.nn.softmax)])
+
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy')
+model.fit(training_images, training_labels, epochs=5)
+model.evaluate(test_images, test_labels)
 
